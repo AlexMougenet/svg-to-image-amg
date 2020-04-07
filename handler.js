b572ea0d645bb0c0a254515045b9dfb75f1769a3
Zaid ALHADAD <zaid.alhadad.ext@biosency.com>
2020-04-07 09:50:47 +0200

    remove not needed packages

--- handler.js
+++ handler.js
@@ -4,9 +4,11 @@ const svg = require('./svg');
 
 module.exports.render = async (event) => {
   const image = event.body;
-
+  console.log("image");
+  console.log(image);
   const png = await svg(image, { width: 2000 });
-
+  console.log("png");
+  console.log(png);
   return {
     statusCode: 200,
     headers: {
